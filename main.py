08f5d6ae5b5ddbcf9d40d2d33eeb34be8fc3e296
luiz826 <luiz.costa@academico.ufpb.br>
2022-06-22 16:09:43 -0300

    Add aspirador R4

--- main.py
+++ main.py
@@ -3,23 +3,36 @@ from dfs import *
 from romenia import *
 from aspirador import *
 
+# PROBLEMA ASPIRADOR R3
 no_inicial = No([0,0,0], 1, None, None)
 problemaAspirador = Problema(no_inicial, (lambda no: no if no.estado == [1,1,1] else None), estadosAspirador)
 
-# bl = BuscaLargura(problemaAspirador)
-# bl.efetuarBusca()
-
+bl = BuscaLargura(problemaAspirador)
 bp = BuscaProfundidade(problemaAspirador)
+print("\nBusca em Largura\n")
+bl.efetuarBusca()
+print("\nBusca em Profundidade\n")
 bp.efetuarBusca()
 
-# no_arad = No('Arad', 0, None, None);
+# PROBLEMA ASPIRADOR R4
+no_inicial = No([0,0,0,0], 1, None, None)
+problemaAspirador = Problema(no_inicial, (lambda no: no if no.estado == [1,1,1,1] else None), estadosAspirador2)
 
-# problemaRomenia = Problema(no_arad, (lambda no: no if no.estado == 'Bucharest' else None), estadosRomenia)
+bl = BuscaLargura(problemaAspirador)
+bp = BuscaProfundidade(problemaAspirador)
+print("\nBusca em Largura\n")
+bl.efetuarBusca()
+print("\nBusca em Profundidade\n")
+bp.efetuarBusca()
 
-# bl = BuscaLargura(problemaRomenia)
-# bp = BuscaProfundidade(problemaRomenia)
+# PROBLEMA ROTA ROMENIA
+no_arad = No('Arad', 0, None, None);
 
-# print("\nBusca em Largura\n")
-# bl.efetuarBusca()
-# print("\nBusca em Profundidade\n")
-# bp.efetuarBusca()
+problemaRomenia = Problema(no_arad, (lambda no: no if no.estado == 'Bucharest' else None), estadosRomenia)
+
+bl = BuscaLargura(problemaRomenia)
+bp = BuscaProfundidade(problemaRomenia)
+print("\nBusca em Largura\n")
+bl.efetuarBusca()
+print("\nBusca em Profundidade\n")
+bp.efetuarBusca()
